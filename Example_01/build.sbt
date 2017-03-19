3938a59493de697e9c41cffe00d6d28131c6624c
godpan <nubug@dripower.com>
2017-03-19 14:07:51 +0800

    add Example_01

---
+++ Example_01/build.sbt
@@ -0,0 +1,11 @@
+name := "Example_01"
+
+version := "1.0"
+
+scalaVersion := "2.11.8"
+
+val akkaVersion   = "2.4.16"
+
+libraryDependencies +=
+  "com.typesafe.akka" %% "akka-actor" % akkaVersion
+    
---
+++ Example_01/src/main/scala/Example_01.scala
@@ -0,0 +1,36 @@
+import akka.actor._
+import akka.event.Logging
+
+/**
+  * Created by panguansen on 17/3/19.
+  */
+trait Action {
+  val message: String
+  val time: Int
+}
+
+case class TurnOnLight(time: Int) extends Action { // 开灯消息
+  val message = "Turn on the living room light"
+}
+
+case class BoilWater(time: Int) extends Action { // 烧水消息
+  val message = "Burn a pot of water"
+}
+
+class RobotActor extends Actor {
+  val log = Logging(context.system, this)
+  def receive: Receive = { //机器人接受指令
+    case t: TurnOnLight => log.info(s"${t.message} after ${t.time} hour")
+    case b: BoilWater => log.info(s"${b.message} after ${b.time} hour")
+    case _ => log.info("I can not handle this message")
+  }
+}
+
+object Example_01 extends App {
+  val actorSyatem = ActorSystem("robot-system") //
+  val robotActor = actorSyatem.actorOf(Props(new RobotActor()), "robotActor") //创建一个机器人
+  robotActor ! TurnOnLight(1) //给机器人发送一个开灯命令
+  robotActor ! BoilWater(2) //给机器人发送一个烧水命令
+  robotActor ! "who are you" //给机器人发送一个任意命令
+  actorSyatem terminate ()
+}
