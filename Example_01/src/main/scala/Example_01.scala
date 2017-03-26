fa5dc3f69a3b69c60d5f24c2e92fe21895e64af3
godpan <nubug@dripower.com>
2017-03-26 08:41:08 +0800

    fix some mistake

--- Example_01/src/main/scala/Example_01.scala
+++ Example_01/src/main/scala/Example_01.scala
@@ -27,10 +27,10 @@ class RobotActor extends Actor {
 }
 
 object Example_01 extends App {
-  val actorSyatem = ActorSystem("robot-system") //
-  val robotActor = actorSyatem.actorOf(Props(new RobotActor()), "robotActor") //创建一个机器人
+  val actorSystem = ActorSystem("robot-system") //
+  val robotActor = actorSystem.actorOf(Props(new RobotActor()), "robotActor") //创建一个机器人
   robotActor ! TurnOnLight(1) //给机器人发送一个开灯命令
   robotActor ! BoilWater(2) //给机器人发送一个烧水命令
   robotActor ! "who are you" //给机器人发送一个任意命令
-  actorSyatem terminate ()
+  actorSystem terminate ()
 }
